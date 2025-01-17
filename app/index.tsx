8bb8b5e5a50d1880f087a6c6b4bf9df486fd9005
Sukhjit Singh <144463305+Sukhjitsingh866@users.noreply.github.com>
2025-01-17 19:31:11 +0800

    update index
    there was a error with route.name that is fixed

--- app/index.tsx
+++ app/index.tsx
@@ -6,36 +6,34 @@ import HabitBuilder from "./Habit Builder"
 import ChatBot from "./Chatbot"
 import AntDesign from '@expo/vector-icons/AntDesign';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 import Plus from '@expo/vector-icons/MaterialCommunityIcons';
 import EvilIcons from '@expo/vector-icons/EvilIcons';
+import { Ionicons } from "@expo/vector-icons"
+import { TabBarIndicator } from "react-native-tab-view"
 //Tab Bottom
 const Tab = createBottomTabNavigator();
-
 function TabGroup() {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen name="ChatBot" component={ChatBot}
-            options={{tabBarIcon: () => <MaterialCommunityIcons name="robot-happy-outline" size={24} color="black" />}}
-            />
-            <Tab.Screen name="Journaling" component={Journaling}
-            options={{tabBarIcon: ()=> <SimpleLineIcons name="notebook" size={24} color="black" />}}
-            />
-            <Tab.Screen name="Home" component={Index}
-            options={{tabBarIcon:()=> <AntDesign name="home" size={24} color="black" />}}
-            />
-            <Tab.Screen name="Habit Builder" component={HabitBuilder}
-            options={{tabBarIcon: ()=> <Plus name="checkbox-marked-circle-plus-outline" size={24} color="black" />}}
-            />
-            <Tab.Screen name="Achievements" component={Achievements}
-            options={{tabBarIcon: ()=> <EvilIcons name="trophy" size={24} color="black" />}}
-            />
-        </Tab.Navigator>
-    )
+  return (
+      <Tab.Navigator>
+          <Tab.Screen name="ChatBot" component={ChatBot}
+          options={{tabBarIcon: ()=> <MaterialCommunityIcons name="robot-excited-outline" size={24} color="black" />}}
+          />
+          <Tab.Screen name="Journaling" component={Journaling} 
+          options={{tabBarIcon: ()=> <Ionicons name="book-outline" size={24} color="black" />}}
+          />
+          <Tab.Screen name="Home" component={Index} 
+          options={{tabBarIcon: ()=> <AntDesign name="home" size={24} color="black" />}}
+          />
+          <Tab.Screen name="Habit Builder" component={HabitBuilder} 
+          options={{tabBarIcon: () => <Plus name="plus-minus-variant" size={24} color="black" />}}
+          />
+          <Tab.Screen name="Achievements" component={Achievements} 
+          options={{tabBarIcon: ()=> <EvilIcons name="trophy" size={24} color="black" />}}
+          />
+      </Tab.Navigator>
+  );
 }
 
 export default function Navigation() {
-    return(
-        <TabGroup/>
-    )
+  return <TabGroup />;
 }
